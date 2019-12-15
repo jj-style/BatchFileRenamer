89159f37fc2a8948796ebbd2a03cef44654abe2c
JJ Style <style.jj@protonmail.com>
2019-12-15 17:55:20 +0000

    use form layout. Generate example file name and allow parathensize enum

--- batch_rename.py
+++ batch_rename.py
@@ -3,10 +3,14 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 class App(QWidget):
-    def __init__(self, width=400, height=250):
+    def __init__(self, width=400, height=350):
         super().__init__()
         self.setWindowTitle('Batch Renamer')
         self.setFixedSize(width, height)
+
+        self.sep_dict = {"underscore":"_", "hyphen":"-", "period":".", "space":" "}
+        self.enum_dict = {"numbers":"1", "letters":"a"}
+
         self.createWidgets()
         self.arrangeWidgets()
         self.makeConnections()
@@ -14,6 +18,8 @@ class App(QWidget):
     def createWidgets(self):
         self.selected_folder = QLabel()
         self.folder_dialog = QFileDialog(self)
+        #self.folder_dialog.setDirectory(os.getenv('HOME'))
+        self.folder_dialog.setDirectory(os.path.join(os.getenv('HOME'),"Desktop","Test"))
         self.folder_dialog.setFileMode(QFileDialog.Directory)
         self.folder_button = QPushButton("&Choose Folder")
 
@@ -24,6 +30,21 @@ class App(QWidget):
         self.new_name_label = QLabel("&Filename template")
         self.new_name = QLineEdit()
         self.new_name_label.setBuddy(self.new_name)
+
+        self.l_unique_separator = QLabel("&Separator")
+        self.unique_separator = QComboBox(self)
+        self.unique_separator.addItems(list(self.sep_dict.keys()))
+        self.l_unique_separator.setBuddy(self.unique_separator)
+
+        self.l_unique_enum = QLabel("&Enumerator")
+        self.unique_enumerator = QComboBox(self)
+        self.unique_enumerator.addItems(list(self.enum_dict.keys()))
+        self.l_unique_enum.setBuddy(self.unique_enumerator)
+
+        self.paranthesize_enum = QCheckBox("&Paranthesize enumerator")
+
+        self.example_name = QLabel()
+        self.submit = QPushButton("Rename!")
 
     def arrangeWidgets(self):
         main = QVBoxLayout()
@@ -42,23 +63,39 @@ class App(QWidget):
         filetype_layout.addStretch(1)
         main.addLayout(filetype_layout)
 
-        new_name_layout = QHBoxLayout()
-        new_name_layout.addWidget(self.new_name_label)
-        new_name_layout.addWidget(self.new_name)
+        form = QFormLayout()
+        form.addRow(self.new_name_label, self.new_name)
+        form.addRow(self.l_unique_separator, self.unique_separator)
+        form.addRow(self.l_unique_enum, self.unique_enumerator)
+        form.addWidget(self.paranthesize_enum)
+        main.addLayout(form)
 
-        main.addLayout(new_name_layout)
-        main.addWidget(self.new_name)
+        example_name_layout = QHBoxLayout()
+        example_name_layout.addWidget(QLabel("Example filename: "))
+        example_name_layout.addWidget(self.example_name)
+        example_name_layout.addStretch(1)
+        main.addLayout(example_name_layout)
+
         main.addStretch(1)
+        main.addWidget(self.submit)
         self.setLayout(main)
+
+    def update_example(self):
+        name = self.new_name.text()
+        sep = self.sep_dict[self.unique_separator.currentText()]
+        enum = self.enum_dict[self.unique_enumerator.currentText()]
+        if self.paranthesize_enum.checkState():
+            enum = f"({enum})"
+        self.example_name.setText(name+sep+enum)
 
     def makeConnections(self):
         self.folder_button.clicked.connect(self.folder_dialog.show)
-        self.folder_dialog.currentChanged.connect(self.selected)
         self.folder_dialog.currentChanged.connect(self.selected_folder.setText)
 
-    def selected(self, path):
-        print("selected: {}".format(path))
-
+        self.new_name.textChanged.connect(self.update_example)
+        self.unique_separator.currentIndexChanged.connect(self.update_example)
+        self.unique_enumerator.currentIndexChanged.connect(self.update_example)
+        self.paranthesize_enum.stateChanged.connect(self.update_example)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
