52707dac16f5c043e85e4bcf0be7b57e0f438b8b
JJ Style <style.jj@protonmail.com>
2019-12-18 09:33:11 +0000

    create setup.py file to install app

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="batch-file-renamer",
+    scripts = [
+        'BatchFileRenamer/batch-file-renamer'
+    ],
+    version="1.0",
+    description="Simple batch file renaming application",
+    author="JJ Style",
+    author_email="style.jj@pm.me",
+    license="MIT",
+    url="https://github.com/jj-style/BatchFileRenamer",
+    python_requires=">=3.6"
+)
